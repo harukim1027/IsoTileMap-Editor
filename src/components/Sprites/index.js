6b2a7d8213751d15576dd24f66d2ddb5ac6a8e4d
김유나 <20162921@gm.hannam.ac.kr>
2024-09-14 14:55:41 +0900

    Feat: Tiles 배열로 추가 및 styled-components Style 폴더 분리 및 추가
    김유나(24.09.14)

--- src/components/Sprites/index.js
+++ src/components/Sprites/index.js
@@ -1,10 +1,21 @@
 import React from "react";
+import tile1 from "../../assets/sources/plains-sliced_21.png";
+import tile2 from "../../assets/sources/plains-sliced_42.png";
+import tile3 from "../../assets/sources/plains-sliced_45.png";
+import tile4 from "../../assets/sources/plains-sliced_74.png";
+import { SpriteGrid, SpriteImage } from "../../styles/SpritesStyles";
+
+const tileImages = [tile1, tile2, tile3, tile4];
 
 const Sprites = () => {
   return (
     <div>
       <h3>Sprites</h3>
-      <div className="sprite-grid"></div>
+      <SpriteGrid>
+        {tileImages.map((src, index) => (
+          <SpriteImage key={index} src={src} alt={`Tile ${index + 1}`} />
+        ))}
+      </SpriteGrid>
     </div>
   );
 };
---
+++ src/styles/SpritesStyles/index.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const SpriteGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 50px);
+  grid-gap: 10px;
+  padding: 10px;
+`;
+
+export const SpriteImage = styled.img`
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.1);
+    transition: transform 0.2s;
+  }
+`;
